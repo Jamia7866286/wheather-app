191b865de725405bf2c7746ffc89b6061e49d17b
Naseem Ahmad <naseem@neurosensum.com>
2021-07-01 09:15:21 +0530

    all react projects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Button } from 'reactstrap';
+import { ToastContainer, toast } from 'react-toastify';
+import Temp from './components/temp'
 
 function App() {
+
+  const notify = () => toast("Wow so easy!");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Button onClick={notify}>Click me</Button>
+      <ToastContainer /> */}
+      <Temp />
+
     </div>
   );
 }
---
+++ src/components/temp.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react'
+import { Form, FormGroup, Input } from 'reactstrap'
+
+const Temp = ()=> {
+
+      const [search, setSearch] = useState('delhi');
+      const [city, setcity] = useState(null);
+
+      useEffect(() => {
+
+            const fetchAPI = async ()=> {
+                  const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=4a36212207f0face69ede65f8d6fa2f6`;
+                  const response = await fetch(url);
+                  const resJSON = await response.json();
+                  console.log(resJSON);
+                  setcity(resJSON.main);
+            }
+            fetchAPI();
+
+      }, [search]);
+      
+      const inputGetVal = (e)=> {
+            const val = e.target.value;
+            setSearch(val);
+      }
+      
+      return (
+            <div className="temp-main">
+                  <div className="temp-box">
+                        <Form>
+                              <FormGroup>
+                                    <Input type="search" value={search} autoFocus name="email" onChange={inputGetVal} />
+                              </FormGroup>
+                        </Form>
+
+                        {!city ? 
+                              <p>No data found</p> : 
+                              <div className="main">
+                                    <h2>
+                                          <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24"/><path d="M6.5,2c1.1,0,2,0.9,2,2s-0.9,2-2,2s-2-0.9-2-2S5.4,2,6.5,2z M15.5,9.5c0,0.83,0.67,1.5,1.5,1.5s1.5-0.67,1.5-1.5 S17.83,8,17,8S15.5,8.67,15.5,9.5z M18.5,12h-2.84c-0.58,0.01-1.14,0.32-1.45,0.86l-0.92,1.32L9.72,8C9.35,7.37,8.69,7.01,8.01,7H5 C3.9,7,3,7.9,3,9v6h1.5v7h5V11.61L12.03,16h2.2L15,14.9V22h4v-5h1v-3.5C20,12.68,19.33,12,18.5,12z"/></g></svg>
+                                          <span>{search}</span>
+                                    </h2>
+                                    <h1>{city.temp} <sup><small>o</small></sup>Cel</h1>
+                                    <small>Min : {city.temp_min} <sup><small>o</small></sup>Cel | Max : {city.temp_max} <sup><small>o</small></sup>Cel </small>
+                              </div>
+                        }
+                       
+                  </div>
+            </div>
+      );
+}
+
+export default Temp
